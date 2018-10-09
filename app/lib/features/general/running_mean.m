e16ac1fd3b7365853b2c765145792dcb27401890
Bl0101 <yacine.mahdid@live.com>
2018-10-08 22:54:46 -0400

    REFACTOR running_mean

--- app/lib/features/general/running_mean.m
+++ app/lib/features/general/running_mean.m
@@ -18,12 +18,12 @@ function [running_avg] = running_mean(type,data_matrix,length_mean,overlap)
     data_length = length(data);
     number_window = floor(time_length/length_mean);
     points_per_window = floor(data_length/number_window);
+    points_per_overlap = floor(data_length/(number_window*overlap));
+    number_window = number_window*overlap;
     for i = 1:number_window-1
-        disp(i);
-       start_index = (i-1)*points_per_window + 1;
-       end_index = (i)*points_per_window;
-       
-       running_avg(i) = mean(data(start_index:end_index));
+        start_index = (i-1)*points_per_overlap + 1;
+        end_index = (i-1)*points_per_overlap + points_per_window;
+        running_avg(i) = mean(data(start_index:end_index));
     end
     
     %% TODO check what to do with the remainder of the data (as we floor down)
