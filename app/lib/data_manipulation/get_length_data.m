0663a757b9285ed3753e907ffcdaa091ae166d52
Bl0101 <yacine.mahdid@live.com>
2018-10-08 22:07:23 -0400

    ADD get length (in sec)

--- app/lib/data_manipulation/get_length_data.m
+++ app/lib/data_manipulation/get_length_data.m
@@ -1,9 +1,9 @@
-function [length] = get_length_data(counter)
+function [time_length] = get_length_data(counter)
 %GET_LENGTH_DATA Summary of this function goes here
 %   Detailed explanation goes here
 %% Need to return the length in seconds
     %May need to convert
-    length = counter(1) - counter(length(counter));
+    time_length = counter(length(counter),1) - counter(1,1);
 
 end
 
