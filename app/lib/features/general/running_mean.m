35bcbe48121ace4e3f5673b2d2d5d190e0788eec
Bl0101 <yacine.mahdid@live.com>
2018-10-08 22:16:46 -0400

    REFACTOR get length

--- app/lib/features/general/running_mean.m
+++ app/lib/features/general/running_mean.m
@@ -1,7 +1,18 @@
 function [running_avg] = running_mean(type,data_matrix,length_mean,overlap)
 %RUNNING_MEAN Summary of this function goes here
 %   Detailed explanation goes here
-    time_length = data_matrix
+    
+    index_type = 0;
+    if(strcmp(type,"bvp"))
+        index_type = 2;
+    elseif(strcmp(type,"sc"))
+        index_type = 3;
+    elseif(strcmp(type,"temp"))
+        index_type = 4;
+    elseif(strcmp(type,"hr"))
+        index_type = 5;
+    end
+    time_length = get_length(data_matrix);
     
 end
 
