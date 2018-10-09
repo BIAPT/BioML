acbe42ccada5492c986388e8e07e748cfeba6d8b
Bl0101 <yacine.mahdid@live.com>
2018-10-09 08:01:45 -0400

    ADD detail in clustering

--- app/experiment/emotional_clustering.m
+++ app/experiment/emotional_clustering.m
@@ -9,12 +9,22 @@
 % Calm: 3.13,3.14,3.15
 
 
+    %% Variables setup
+    
+
 %% Load the raw data (From a folder) and iterate through each participant
 %  For now we will only load one data file and play with it
 
-%% Calculate the features and return a matrix
-X = calculate_features(raw_data);
-
-%% Run the K-means
-idx = kmeans(X,k)
+    current_participant_id = 1;
+    %% Calculate the features and return a matrix
+    X = calculate_features(raw_data);
+    sample_id = repmat(current_participant_id,[length(X) 1])
+    
+    Y = [Y;sample_id];
+    
+    %% Run the K-means
+    idx = kmeans(X,k)
+    % idx will contain the cluster belongings to a given sample!
+    
+    %% Assign a cluster to a participant depending on its ID
 
