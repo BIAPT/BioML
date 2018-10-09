9b848258fd2efaddc41ac5681cade7257c56bc64
Bl0101 <yacine.mahdid@live.com>
2018-10-08 22:59:32 -0400

    ADD calculate feature for the 4 types of means

--- app/lib/features/calculate_features.m
+++ app/lib/features/calculate_features.m
@@ -2,12 +2,19 @@ function [X] = calculate_features(raw_data)
 %CALUCLATE_FEATURES Summary of this function goes here
 %   Detailed explanation goes here
 
+    %% Variables Declaration
+    mean_window_size = 4;
+    mean_window_overlap = 2;
+
     %% Blood Volume Pulse Features
-    
+    bvp_mean = running_mean("bvp",raw_data,mean_window_size,mean_window_overlap);
     %% Skin Conductance Features
-    
+    sc_mean = running_mean("sc",raw_data,mean_window_size,mean_window_overlap);
     %% Temperature Features
-    
+    temp_mean = running_mean("temp",raw_data,mean_window_size,mean_window_overlap);
     %% Heart Rate Features
+    hr_mean = running_mean("hr",raw_data,mean_window_size,mean_window_overlap);
+    
+    X = [bvp_mean;sc_mean;temp_mean;hr_mean]';
 end
 
