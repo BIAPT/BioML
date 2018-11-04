1c69762b980aa2addc09a4dfd46b83209aae6a7f
Bl0101 <yacine.mahdid@live.com>
2018-11-04 12:19:41 -0500

    ADD optimized iteration backbone

---
+++ app/lib/features/helper/iterate_over_windows.m
@@ -0,0 +1,13 @@
+function [running_features] = iterate_over_windows(type,data_matrix,windows_size,overlap,analysis_techniques)
+%ITERATE_OVER_WINDOWS Summary of this function goes here
+%   Input:
+%       type: bvp,hr,sc,temp
+%       data_matrix: the full dataframe
+%       windows_size: size on which we want to calculate the analysis
+%       techniques
+%       overlap: overlap between each window
+%   Output:
+%       running_feature: structure containing vectors of running features
+
+end
+
